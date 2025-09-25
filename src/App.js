404b226b3945990ededb550bd12a7f942ae0b564
Shivam Karpe <64491477+shivamk19@users.noreply.github.com>
2025-09-25 13:37:22 +0530

    Create App.js
    update

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PostsApp from './components/PostsApp';
+
+function App() {
+  return (
+    <div className="App">
+      <PostsApp />
+    </div>
+  );
+}
+
+export default App;
